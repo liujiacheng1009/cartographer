372631bdecb56fd8b3d6a64ca67f248fc2cd6b36
jesse <liujiacheng1009@gmail.com>
2026-08-22 13:16:38 +0800

    refactor: inline single-use scan matching costs

--- slam/ceres_scan_matcher_2d.cc
+++ slam/ceres_scan_matcher_2d.cc
@@ -24,8 +24,135 @@
 #include "cartographer/core/parameter_dictionary.h"
 #include "cartographer/slam/grid_2d.h"
 #include "cartographer/slam/occupied_space_cost_function_2d.h"
-#include "cartographer/slam/rotation_delta_cost_functor_2d.h"
-#include "cartographer/slam/translation_delta_cost_functor_2d.h"
+/*
+ * Copyright 2016 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CARTOGRAPHER_MAPPING_INTERNAL_2D_SCAN_MATCHING_ROTATION_DELTA_COST_FUNCTOR_2D_H_
+#define CARTOGRAPHER_MAPPING_INTERNAL_2D_SCAN_MATCHING_ROTATION_DELTA_COST_FUNCTOR_2D_H_
+
+#include "Eigen/Core"
+#include "ceres/ceres.h"
+
+namespace cartographer {
+namespace mapping {
+namespace scan_matching {
+
+// Computes the cost of rotating 'pose' to 'target_angle'. Cost increases with
+// the solution's distance from 'target_angle'.
+class RotationDeltaCostFunctor2D {
+ public:
+  static ceres::CostFunction* CreateAutoDiffCostFunction(
+      const double scaling_factor, const double target_angle) {
+    return new ceres::AutoDiffCostFunction<
+        RotationDeltaCostFunctor2D, 1 /* residuals */, 3 /* pose variables */>(
+        new RotationDeltaCostFunctor2D(scaling_factor, target_angle));
+  }
+
+  template <typename T>
+  bool operator()(const T* const pose, T* residual) const {
+    residual[0] = scaling_factor_ * (pose[2] - angle_);
+    return true;
+  }
+
+ private:
+  explicit RotationDeltaCostFunctor2D(const double scaling_factor,
+                                      const double target_angle)
+      : scaling_factor_(scaling_factor), angle_(target_angle) {}
+
+  RotationDeltaCostFunctor2D(const RotationDeltaCostFunctor2D&) = delete;
+  RotationDeltaCostFunctor2D& operator=(const RotationDeltaCostFunctor2D&) =
+      delete;
+
+  const double scaling_factor_;
+  const double angle_;
+};
+
+}  // namespace scan_matching
+}  // namespace mapping
+}  // namespace cartographer
+
+#endif  // CARTOGRAPHER_MAPPING_INTERNAL_2D_SCAN_MATCHING_ROTATION_DELTA_COST_FUNCTOR_2D_H_
+/*
+ * Copyright 2016 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CARTOGRAPHER_MAPPING_INTERNAL_2D_SCAN_MATCHING_TRANSLATION_DELTA_COST_FUNCTOR_2D_H_
+#define CARTOGRAPHER_MAPPING_INTERNAL_2D_SCAN_MATCHING_TRANSLATION_DELTA_COST_FUNCTOR_2D_H_
+
+#include "Eigen/Core"
+#include "ceres/ceres.h"
+
+namespace cartographer {
+namespace mapping {
+namespace scan_matching {
+
+// Computes the cost of translating 'pose' to 'target_translation'.
+// Cost increases with the solution's distance from 'target_translation'.
+class TranslationDeltaCostFunctor2D {
+ public:
+  static ceres::CostFunction* CreateAutoDiffCostFunction(
+      const double scaling_factor, const Eigen::Vector2d& target_translation) {
+    return new ceres::AutoDiffCostFunction<TranslationDeltaCostFunctor2D,
+                                           2 /* residuals */,
+                                           3 /* pose variables */>(
+        new TranslationDeltaCostFunctor2D(scaling_factor, target_translation));
+  }
+
+  template <typename T>
+  bool operator()(const T* const pose, T* residual) const {
+    residual[0] = scaling_factor_ * (pose[0] - x_);
+    residual[1] = scaling_factor_ * (pose[1] - y_);
+    return true;
+  }
+
+ private:
+  // Constructs a new TranslationDeltaCostFunctor2D from the given
+  // 'target_translation' (x, y).
+  explicit TranslationDeltaCostFunctor2D(
+      const double scaling_factor, const Eigen::Vector2d& target_translation)
+      : scaling_factor_(scaling_factor),
+        x_(target_translation.x()),
+        y_(target_translation.y()) {}
+
+  TranslationDeltaCostFunctor2D(const TranslationDeltaCostFunctor2D&) = delete;
+  TranslationDeltaCostFunctor2D& operator=(
+      const TranslationDeltaCostFunctor2D&) = delete;
+
+  const double scaling_factor_;
+  const double x_;
+  const double y_;
+};
+
+}  // namespace scan_matching
+}  // namespace mapping
+}  // namespace cartographer
+
+#endif  // CARTOGRAPHER_MAPPING_INTERNAL_2D_SCAN_MATCHING_TRANSLATION_DELTA_COST_FUNCTOR_2D_H_
 #include "cartographer/core/transform.h"
 #include "ceres/ceres.h"
 #include "glog/logging.h"
