d9994f194821ea045d7f89df757ea7d8d3d5d9c8
jesse <liujiacheng1009@gmail.com>
2026-08-22 13:25:51 +0800

    refactor: merge probability range insertion

--- slam/submap_2d.h
+++ slam/submap_2d.h
@@ -25,7 +25,69 @@
 #include "cartographer/slam/grid_2d.h"
 #include "cartographer/slam/grid_2d.h"
 #include "cartographer/slam/options.h"
-#include "cartographer/slam/probability_grid_range_data_inserter_2d.h"
+/*
+ * Copyright 2016 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CARTOGRAPHER_MAPPING_2D_RANGE_DATA_INSERTER_2D_PROBABILITY_GRID_H_
+#define CARTOGRAPHER_MAPPING_2D_RANGE_DATA_INSERTER_2D_PROBABILITY_GRID_H_
+
+#include <utility>
+#include <vector>
+
+#include "cartographer/core/parameter_dictionary.h"
+#include "cartographer/core/port.h"
+#include "cartographer/slam/grid_2d.h"
+#include "cartographer/slam/grid_2d.h"
+#include "cartographer/slam/options.h"
+#include "cartographer/core/point_cloud.h"
+#include "cartographer/core/range_data.h"
+
+namespace cartographer {
+namespace mapping {
+
+ProbabilityGridRangeDataInserterOptions2D
+CreateProbabilityGridRangeDataInserterOptions2D(
+    common::ParameterDictionary* parameter_dictionary);
+RangeDataInserterOptions CreateRangeDataInserterOptions(
+    common::ParameterDictionary* parameter_dictionary);
+
+class ProbabilityGridRangeDataInserter2D {
+ public:
+  explicit ProbabilityGridRangeDataInserter2D(
+      const ProbabilityGridRangeDataInserterOptions2D& options);
+
+  ProbabilityGridRangeDataInserter2D(
+      const ProbabilityGridRangeDataInserter2D&) = delete;
+  ProbabilityGridRangeDataInserter2D& operator=(
+      const ProbabilityGridRangeDataInserter2D&) = delete;
+
+  // Inserts 'range_data' into 'probability_grid'.
+  void Insert(const sensor::RangeData& range_data,
+              ProbabilityGrid* probability_grid) const;
+
+ private:
+  const ProbabilityGridRangeDataInserterOptions2D options_;
+  const std::vector<uint16> hit_table_;
+  const std::vector<uint16> miss_table_;
+};
+
+}  // namespace mapping
+}  // namespace cartographer
+
+#endif  // CARTOGRAPHER_MAPPING_2D_RANGE_DATA_INSERTER_2D_PROBABILITY_GRID_H_
 #include "cartographer/slam/submaps.h"
 #include "cartographer/slam/trajectory_node.h"
 #include "cartographer/slam/grid_2d.h"
